210ca79384ceba48ecaf147b2f62f2b59a47caac
Stanley Hayford <stanley_hayford@vienhealth.com>
2021-08-03 18:20:52 +0000

    Add hotel room model

---
+++ src/models/hotel-room.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const hotelRoomSchema = new Schema({
+    number: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    hotel: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Hotel'
+    },
+    bedCount: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    price: {
+        amount: {
+            type: Number
+        },
+        currency: {
+            type: String
+        }
+    },
+    gallery: {
+        type: [String]
+    },
+    image: {
+        type: String
+    },
+    addons: {
+        type: [String]
+    }
+}, {timestamps: {createdAt: true, updatedAt: true}});
+
+const HotelRoom = mongoose.model('HotelRoom', hotelRoomSchema);
+
+module.exports = HotelRoom;
